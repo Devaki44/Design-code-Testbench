d0d4dadc658b4d0982bfc02e816e7110fde3d9bf
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2024-09-03 08:15:09 -0700

    Create norgate.v

---
+++ Basics gates/norgate.v
@@ -0,0 +1,22 @@
+//Design code
+module norg(x,y,z);
+  input x,y;
+  output z;
+  assign z=~(x|y);
+endmodule
+
+
+//Testbench
+module norg_tb;
+   reg x,y;
+   wire z;
+norg DUT(x,y,z);
+  initial begin
+    $monitor("At time =%0t:x=%b y=%b,z=%b",$time,x,y,z);
+     x=1'b0;y=1'b0;#1
+     x=1'b0;y=1'b1;#1
+     x=1'b1;y=1'b0;#1
+     x=1'b1;y=1'b1;#1
+    $finish;
+  end
+endmodule
