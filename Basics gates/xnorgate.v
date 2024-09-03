0670b924868ecdfbf12a5513eb6b289b9c6e8391
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2024-09-03 08:20:28 -0700

    Create xnorgate.v

---
+++ Basics gates/xnorgate.v
@@ -0,0 +1,23 @@
+//Design code
+module xnorg(x,y,z);
+  input x,y;
+  output z;
+  assign z=~(x^y);
+endmodule
+
+
+//Testbench
+module xnorg_tb;
+   reg x,y;
+   wire z;
+xnorg DUT(x,y,z);
+  initial begin
+    $monitor("At time =%0t:x=%b y=%b,z=%b",$time,x,y,z);
+     x=1'b0;y=1'b0;#1
+     x=1'b0;y=1'b1;#1
+     x=1'b1;y=1'b0;#1
+     x=1'b1;y=1'b1;#1
+    $finish;
+  end
+endmodule
+    
