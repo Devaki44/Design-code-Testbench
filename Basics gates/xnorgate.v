699db45213725f12b63d967d4dfbb82608b48848
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2024-09-03 08:40:07 -0700

    Update xnorgate.v

--- Basics gates/xnorgate.v
+++ Basics gates/xnorgate.v
@@ -12,6 +12,8 @@ module xnorg_tb;
    wire z;
 xnorg DUT(x,y,z);
   initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
     $monitor("At time =%0t:x=%b y=%b,z=%b",$time,x,y,z);
      x=1'b0;y=1'b0;#1
      x=1'b0;y=1'b1;#1
