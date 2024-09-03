85ab40d6185292bd9dd6e29b495f71d2d65c4236
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2024-09-03 08:38:34 -0700

    Update nandgate.v

--- Basics gates/nandgate.v
+++ Basics gates/nandgate.v
@@ -12,6 +12,8 @@ module nandg_tb;
    wire z;
 nandg DUT(x,y,z);
   initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
     $monitor("At time =%0t:x=%b y=%b,z=%b",$time,x,y,z);
      x=1'b0;y=1'b0;#1
      x=1'b0;y=1'b1;#1
