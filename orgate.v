2e7b4a0b980a8980393837f4bf2ce74a81f2401e
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2024-09-03 08:05:38 -0700

    Create orgate.v

---
+++ orgate.v
@@ -0,0 +1,21 @@
+//Design code
+module org(x,y,z);
+  input x,y;
+  output z;
+  assign z=x|y;
+endmodule
+
+//Testbench
+module org_tb;
+   reg x,y;
+   wire z;
+org DUT(x,y,z);
+  initial begin
+    $monitor("At time =%0t:x=%b y=%b,z=%b",$time,x,y,z);
+     x=1'b0;y=1'b0;#1
+     x=1'b0;y=1'b1;#1
+     x=1'b1;y=1'b0;#1
+     x=1'b1;y=1'b1;#1
+    $finish;
+  end
+endmodule
