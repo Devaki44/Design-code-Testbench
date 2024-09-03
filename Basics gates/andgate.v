96af6aecef5d1d3d1c0c90ed2e642b004a71ac6e
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2024-09-03 08:37:54 -0700

    Update andgate.v

--- Basics gates/andgate.v
+++ Basics gates/andgate.v
@@ -11,6 +11,8 @@ module andg_tb;
    wire z;
 andg DUT(x,y,z);
   initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
     $monitor("At time =%0t:x=%b y=%b,z=%b",$time,x,y,z);
      x=1'b0;y=1'b0;#1
      x=1'b0;y=1'b1;#1
